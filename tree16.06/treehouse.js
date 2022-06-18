c1d4a2e9c751e534acf9a78a7deda3fb9f0567e3
kamilson1 <106776336+kamilson1@users.noreply.github.com>
2022-06-18 13:31:37 +0100

    Add files via upload
    ,

---
+++ tree16.06/treehouse.js
@@ -0,0 +1,26 @@
+let btnh = document.querySelector(".hero--btn");
+btnh.type = "submit";
+btnh.name = "formBtn";
+btnh.addEventListener("click", function () {
+  alert("chuj ci w dupę frajerze");
+});
+
+const hamBurger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav__ul");
+
+hamBurger.addEventListener("click", function () {
+  hamBurger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav__a").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamBurger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+
+window.addEventListener("resize", function() {
+  if (window.innerWidth < 400) resize.classList.remove("hero-span-2");
+});
